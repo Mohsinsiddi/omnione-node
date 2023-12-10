66664925e85b995e1d35094d260e18cae90f2b6d
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2023-12-10 08:46:32 +0530

    latest code

--- functions/constants/RPCs.js
+++ functions/constants/RPCs.js
@@ -1,17 +1,17 @@
 const RPC_MAP = {
-  11155111: "",
-  80001: "",
+  11155111:
+    "https://eth-sepolia.g.alchemy.com/v2/SknoINsvb5mlZyM5PLf-62qDu4Wpht66",
   1442: "https://rpc.public.zkevm-test.net",
-  421613: "",
-  314159: "",
-  534351: "",
-  5001: "",
-  84531: "",
-  51: "",
-  7001: "",
-  195: "",
-  59140: "",
-  44787: "",
+  421613: "https://arbitrum-goerli.publicnode.com",
+  314159: "https://calibration.filfox.info/rpc/v1",
+  534351: "https://sepolia-rpc.scroll.io",
+  5001: "https://rpc.testnet.mantle.xyz",
+  84531: "https://goerli.base.org",
+  51: "https://rpc.apothem.network",
+  7001: "https://zetachain-athens-evm.blockpi.network/v1/rpc/public",
+  195: "https://x1testrpc.okx.com",
+  59140: "https://rpc.goerli.linea.build",
+  44787: "https://alfajores-forno.celo-testnet.org",
 };
 
 module.exports = { RPC_MAP };
--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ const Web3 = require("web3");
 const { ADDR_MAP } = require("./constants/constant");
 const { RPC_MAP } = require("./constants/RPCs");
 const FACTORY = require("./constants/ABI.json");
+const MYNFT = require("./constants/MyNFT.json");
 
 dotenv.config();
 
@@ -124,5 +125,43 @@ exports.depositNFT = functions.firestore
         const receipt = await web3ZkEVM.eth.sendTransaction(txData);
         console.log("Receipt", receipt.transactionHash);
       }
+    } else if (action === "2") {
+      console.log(action);
+      const { tokenID, sender, uri } = snap.data();
+      const adminPrivKey = process.env.PRIVATE_KEY;
+      // EVM PROVIDER && EVM_BRIDGE CONTRACT INIT
+      const provider = new HDWalletProvider({
+        mnemonic: process.env.MNEMONICS,
+        providerOrUrl: RPC_MAP["1442"],
+        pollingInterval: 8000,
+      });
+      const web3ZkEVM = new Web3(provider);
+
+      const { address: adminZkEVM } =
+        web3ZkEVM.eth.accounts.wallet.add(adminPrivKey);
+
+      const zkEVMBridgeInstance = new web3ZkEVM.eth.Contract(
+        MYNFT.abi,
+        "0xC4F3998B5FC10d191cAA7AC31C2443C8Aa87592A"
+      );
+
+      const tx = zkEVMBridgeInstance.methods.mint(tokenID, sender, uri);
+      const [gasPrice, gasCost] = await Promise.all([
+        web3ZkEVM.eth.getGasPrice(),
+        tx.estimateGas({ from: adminZkEVM }),
+      ]);
+
+      const data = tx.encodeABI();
+
+      const txData = {
+        from: adminZkEVM,
+        to: zkEVMBridgeInstance.options.address,
+        data,
+        gas: gasCost,
+        gasPrice,
+      };
+
+      const receipt = await web3ZkEVM.eth.sendTransaction(txData);
+      console.log("MYNFT mint Receipt", receipt.transactionHash);
     }
   });
